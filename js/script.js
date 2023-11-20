ef879774f09e871da1224b8faf3865aee9ec9f7e
SquallStrifeX <squallstrifex@gmail.com>
2023-11-20 17:32:34 +0100

    end exercise +  bonus

--- js/script.js
+++ js/script.js
@@ -18,10 +18,25 @@ else if(eta_passeggero > 65 ) {
 }
 
 else {
-    prezzo_biglietto_con_km = prezzo_biglietto_finale
+    prezzo_biglietto_finale = prezzo_biglietto_con_km 
 }
 
 prezzo_biglietto_finale = prezzo_biglietto_finale.toFixed(2)
 
 console.log(prezzo_biglietto_finale)
 
+
+document.getElementById("costo_finale").innerHTML = prezzo_biglietto_finale
+console.log(prezzo_biglietto_finale)
+
+prezzo_biglietto_finale = Number(prezzo_biglietto_finale)
+
+let prezzo_biglietto_con_sella = (prezzo_biglietto_finale) + (prezzo_biglietto_finale * 0.20);
+
+document.getElementById("costo_finale2").innerHTML = prezzo_biglietto_con_sella
+console.log(prezzo_biglietto_con_sella)
+
+let prezzo_biglietto_con_carrozza = prezzo_biglietto_finale + 22;
+
+document.getElementById("costo_finale3").innerHTML = prezzo_biglietto_con_carrozza
+console.log(prezzo_biglietto_con_carrozza)
